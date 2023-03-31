192b57ca583afef9c5ac4f454029c0f45e932098
Jakub Rada <127985335+kubarada@users.noreply.github.com>
2023-03-31 07:11:31 +0200

    code update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from bs4 import BeautifulSoup
 import os
-import xml.etree.ElementTree as ET
-import re
+import numpy as np
 
-folder_path = 'documents'
+folder_path = 'szjp_sp1-main/documents'
 
 # List all files in the folder
+
 files = os.listdir(folder_path)
 data = list()
 # Loop through each file in the folder and read its contents
@@ -26,30 +26,50 @@ sparse_doc_term_matrix_1 = tfidf_1.fit_transform(data)
 dense_doc_term_matrix_1 = sparse_doc_term_matrix_1.toarray()
 index = tfidf_1.get_feature_names_out()
 
+
+with open('szjp_sp1-main/query_devel.xml', 'r') as f:
+    html_doc = f.read()
+    soup = BeautifulSoup(html_doc, 'html.parser')
+
+    # Get the text from the <ALL> tag
+    doc_text = soup.find('all').get_text()
+
+# Print the plain text
+doc_lines = doc_text.splitlines()
+doc_lines = [x for x in doc_lines if x != '']
+doc_lines = [x for x in doc_lines if x != ' ']
+
+result = []
+current_text = ''
+for item in doc_lines:
+    if item.strip().isdigit():
+        if current_text != '':
+            result.append(current_text.strip())
+            current_text = ''
+        result.append(item.strip())
+    else:
+        current_text += ' ' + item
+
+if current_text != '':
+    result.append(current_text.strip())
+
+print(result)
+
+query_dict = {}
+for i in range(0, len(result), 2):
+    key = result[i]
+    value = result[i+1]
+    query_dict[key] = value
+
+print(query_dict)
+query_list = query_dict.values()
+print(query_list)
+
+
+q=tfidf_1.transform(query_list)       ### !!
+
 print(index)
-print(dense_doc_term_matrix_1)
+dense_q=q.toarray()
+print(dense_q)
 
-with open('query_devel.xml', 'r') as f:
-    data = f.read()
 
-# Passing the stored data inside
-# the beautifulsoup parser, storing
-# the returned object
-Bs_data = BeautifulSoup(data, "xml")
-
-# Finding all instances of tag
-# `unique`
-b_unique = Bs_data.find_all()
-
-print(str(b_unique[1]).replace('<DOCNO> ' + str(1) + ' </DOCNO>', '').split('\n'))
-print(type(b_unique))
-
-# Using find() to extract attributes
-# of the first instance of the tag
-# b_name = Bs_data.find('DOC')
-#
-# print(b_name)
-
-# Extracting the data stored in a
-# specific attribute of the
-# `child` tag
